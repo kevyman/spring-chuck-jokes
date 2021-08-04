2e22b57655a0b24e3914f61d766c2269305beed5
John Wilkos <johnwilkosmail@gmail.com>
2021-08-04 12:39:15 +0200

    Initial commit.

---
+++ src/main/java/com/jwalterwilkos/springchuckjokes/SpringChuckJokesApplication.java
@@ -0,0 +1,13 @@
+package com.jwalterwilkos.springchuckjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChuckJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChuckJokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwalterwilkos/springchuckjokes/SpringChuckJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwalterwilkos.springchuckjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChuckJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
